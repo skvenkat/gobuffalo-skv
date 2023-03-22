c5de476d9257d3bc2a7a1c848eedc8107edea44b
skvuser <skvuser@home-lab.io>
2023-03-22 22:30:09 +0000

    Initial Commit

---
+++ actions/home.go
@@ -0,0 +1,18 @@
+package actions
+
+import (
+	"net/http"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+// HomeHandler is a default handler to serve up
+// a home page.
+func HomeHandler(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
+}
+
+// Custom handler added by me
+func CustomHandler(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.JSON(map[string]string{"response": "This API is powered by GoBuffalo"}))
+}
